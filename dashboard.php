461ba83fc1845fb7fb0f68e4ebdf8997caa1e13f
friendisofyan <friendisofyann@gmail.com>
2021-04-06 18:15:26 +0700

    menambah responsive dropdown menu dari dashboard_admin

--- dashboard.php
+++ dashboard.php
@@ -11,29 +11,38 @@
 <body>
   <div id="main-wrapper">
   <div class="sidebar">
-      <header>
-        <h2>Sistem Presensi</h2>
-      </header>
-      <nav>
-          <div class="utility"><a href="#">
-            <img src="img/user_icon.png" alt="user" >
-            Display Name
-          </a>
-          <a href="#">
-            <img src="img/chart_icon_flip.png" alt="chart">
-            Data Showcase
-          </a>
-          <a href="#">
-            <img src="img/gear_icon_flip.png" alt="control">  
-            Control
-          </a></div>
-          
-          <div class="logout"><a href="includes/logout.inc.php">
-            <img src="img/logout_icon.png" alt="logout">
-            Logout
-          </a></div>
-      </nav>
+    <header>
+      <h2>Sistem Presensi</h2>
+    </header>
+    <div class="dropdown">
+      <button class="dropbtn">Menu</button>
+      <div class="dropdown-content">
+        <nav>
+          <div class="utility">
+            <a href="#">
+             <img src="img/user_icon.png" alt="user" >
+             Display Name
+            </a>
+            <a href="#">
+              <img src="img/chart_icon_flip.png" alt="chart">
+              Data Showcase
+            </a>
+            <a href="#">
+              <img src="img/gear_icon_flip.png" alt="control">  
+              Control
+            </a>
+          </div>
+            
+          <div class="logout">
+            <a href="includes/logout.inc.php">
+              <img src="img/logout_icon.png" alt="logout">
+              Logout
+            </a>
+          </div>
+        </nav>
+      </div>
     </div>
+  </div>
 
     <div class="content-wrapper">
     <center><h1>INI DASHBOARD USER</h1></center>
