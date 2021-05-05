436fb0aa3efb0241a00e8b102268d69cb825fc92
friendisofyan <friendisofyann@gmail.com>
2021-05-05 16:30:14 +0700

    menambah table pegawai, presensi, dan absensi serta relationalnya semua di (update=cascade) (delete=restrict)

--- includes/dbinit.inc.php
+++ includes/dbinit.inc.php
@@ -5,6 +5,9 @@ $dBUsername = "root";
 $dBPassword = "";
 $dBName = "capacitance";
 $tableusers = "users";
+$tablepegawai = "pegawai";
+$tablepresensi = "presensi";
+$tableabsensi = "absensi";
 
 // Create connection
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword);
@@ -33,15 +36,74 @@ echo "Connected successfully to db <br>";
 
 // Create users table
 $sql = "CREATE TABLE IF NOT EXISTS $tableusers (
-  usersId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  usersName VARCHAR(128) NOT NULL,
-  usersJabatan VARCHAR(128) NOT NULL,
-  usersEmail VARCHAR(128) NOT NULL,
-  usersUid VARCHAR(128) NOT NULL,
-  usersPwd VARCHAR(128) NOT NULL
-  )";
+        usersId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        usersName VARCHAR(128) NOT NULL,
+        usersJabatan VARCHAR(128) NOT NULL,
+        usersEmail VARCHAR(128) NOT NULL,
+        usersUid VARCHAR(128) NOT NULL,
+        usersPwd VARCHAR(128) NOT NULL,
+        INDEX (usersName),
+        INDEX (usersEmail)
+        )";
 if (mysqli_query($conn, $sql)) {
   echo "Table users created successfully <br>";
+} 
+else {
+  echo "Error creating table: " . mysqli_error($conn) . " <br>";
+}
+
+
+// Create pegawai table
+$sql = "CREATE TABLE IF NOT EXISTS $tablepegawai (
+        pgwId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        nama VARCHAR(128) NOT NULL,
+        jabatan VARCHAR(128) NOT NULL,
+        email VARCHAR(128) NOT NULL,
+        gender ENUM('male', 'female'),
+        tglLahir VARCHAR(32),
+        telp VARCHAR(16),
+        alamat VARCHAR(128),
+        statusPgw ENUM('aktif',  'keluar'),
+        FOREIGN KEY (nama) REFERENCES users(usersName),
+        FOREIGN KEY (email) REFERENCES users(usersEmail)
+        )";
+if (mysqli_query($conn, $sql)) {
+  echo "Table pegawai created successfully <br>";
+} 
+else {
+  echo "Error creating table: " . mysqli_error($conn) . " <br>";
+}
+
+//create presensi table
+$sql = "CREATE TABLE IF NOT EXISTS $tablepresensi (
+        prsnId INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        pgwId INT(10) NOT NULL UNSIGNED,
+        prsnTgl DATE NOT NULL,
+        jamMasuk TIME,
+        jamKeluar TIME,
+        overtime  TIME,
+        FOREIGN KEY (pgwID) REFERENCES pegawai(pgwID) ON DELETE RESTRICT ON UPDATE CASCADE
+        )";
+if (mysqli_query($conn, $sql)) {
+  echo "Table presensi created successfully <br>";
+} 
+else {
+  echo "Error creating table: " . mysqli_error($conn) . " <br>";
+}
+
+
+//create absensi table
+$sql = "CREATE TABLE IF NOT EXISTS $tableabsensi (
+	      absnId INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        pgwId INT(10) NOT NULL UNSIGNED,
+        tgl_mulai DATE,
+        tgl_selesai DATE,
+        absnStatus enum('cuti', 'ijin', 'sakit'),
+        absnKet VARCHAR(128),
+        FOREIGN KEY (pgwID) REFERENCES pegawai(pgwID) ON DELETE RESTRICT ON UPDATE CASCADE
+        )";
+if (mysqli_query($conn, $sql)) {
+  echo "Table absensi created successfully <br>";
 } 
 else {
   echo "Error creating table: " . mysqli_error($conn) . " <br>";
