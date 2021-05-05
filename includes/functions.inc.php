76af7992dd9e11a4b93523cc393dc77edd360997
friendisofyan <friendisofyann@gmail.com>
2021-05-05 19:25:54 +0700

    menambah query inset (nama, jabatan, email) ke table pegawai di dalam fungsi createUser

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -57,17 +57,22 @@ function uidExist($conn, $username, $email){
 }
 
 function createUser($conn, $name, $jabatan, $email, $username, $pwd){
-  $sql = "INSERT INTO users (usersName, usersJabatan, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?, ?);";
-  $stmt = mysqli_stmt_init($conn);
-  if (!mysqli_stmt_prepare($stmt, $sql)) {
+  $sqlUser = "INSERT INTO users (usersName, usersJabatan, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?, ?);";
+  $sqlPegawai = "INSERT INTO pegawai (nama, jabatan, email) VALUES (?, ?, ?);";
+  $stmt1 = mysqli_stmt_init($conn);
+  $stmt2 = mysqli_stmt_init($conn);
+  if ((!mysqli_stmt_prepare($stmt1, $sqlUser)) || (!mysqli_stmt_prepare($stmt2, $sqlPegawai))) {
     header("location: ../signup.php?error=stmtFailed");
     exit();
   }
 
   $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-  mysqli_stmt_bind_param($stmt, "sssss", $name, $jabatan, $email, $username, $hashedPwd);
-  mysqli_stmt_execute($stmt);
-  mysqli_stmt_close($stmt);
+  mysqli_stmt_bind_param($stmt1, "sssss", $name, $jabatan, $email, $username, $hashedPwd);
+  mysqli_stmt_bind_param($stmt2, "sss", $name, $jabatan, $email);
+  mysqli_stmt_execute($stmt1);
+  mysqli_stmt_execute($stmt2);
+  mysqli_stmt_close($stmt1);
+  mysqli_stmt_close($stmt2);
   header("location: ../signup.php?error=none");
   exit();
 }
