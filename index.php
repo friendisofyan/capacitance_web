dfd55707bba9412bd6f9621762fa84179877a304
friendisofyan <friendisofyann@gmail.com>
2021-05-09 14:17:41 +0700

    mengubah index.php langsung ke page login

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
 <?php
     include_once 'includes/loggedin.inc.php';
-    include_once 'header.php';
-?>
-
-
-<div class="row">
-    <div class="col-2" style="background-color: #EDEBEF">
-        Kolom 1
-    </div>
-
-    <div class="col-8">
-        <center>bagian isi    
-    </div>
-
-    <div class="col-2" style="background-color: #EDEBEF">
-        kolom 3
-    </div> 
-</div>
-
-
-
-<?php
-    include_once 'footer.php';
-?>
+    header("location: login.php");
 
