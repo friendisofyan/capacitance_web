7ce92ced25bbb56fa94cf80370345f6f24a69d53
friendisofyan <friendisofyann@gmail.com>
2021-06-25 00:54:16 +0700

    menambah cekJumlahHadir apabila hadir lebih dari sekali maka hanya menghapus dan tidak memasukkan ke tabel absen

--- dashboard_admin/includes/action_presensi.inc.php
+++ dashboard_admin/includes/action_presensi.inc.php
@@ -11,14 +11,30 @@ if ($_POST['action'] == 'delete') {
     $nama = $id_array[2];
     $tgl = $id_array[3];
 
+    $jumlahHadir = cekJumlahHadir($conn, $pgwId, $tgl);
+
     $sql1 = mysqli_query($conn, "DELETE FROM presensi WHERE prsnId = '$prsnId'");
-    $sql2 = mysqli_query($conn, "INSERT INTO absensi (pgwId, nama, absnTgl)
+    //jika jumlah hadir cuma satu maka akan menambahnya ke tabel absensi
+    //jika lebih dari 1 maka karyawan tersebut masih hadir pada tanggal itu
+    //dan tidak dimasukkan ke tabel absensi
+    if ($jumlahHadir == 1) {
+      $sql2 = mysqli_query($conn, "INSERT INTO absensi (pgwId, nama, absnTgl)
             VALUES ('$pgwId', '$nama', '$tgl')");
+    }
+    
 
     echo json_encode($_POST);
   }
   else{
     echo "Pegawai tidak ada data kehadiran pada tanggal tersebut";
   }
-  
+}
+
+function cekJumlahHadir($conn, $pgwId, $tgl){
+  $sql = "SELECT pgwId FROM presensi 
+          WHERE pgwId='$pgwId' AND prsnTgl='$tgl' ";
+  $result = mysqli_query($conn, $sql);
+  $jumlahHadir = mysqli_num_rows($result);
+  mysqli_free_result($result);
+  return $jumlahHadir;
 }
